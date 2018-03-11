cef1a514ee0bf8e54316b6f61962843afffa3b5a
RegitzeHess <regitzehess@gmail.com>
2018-03-11 12:45:04 +0100

    OutputTest

---
+++ Microwave.Test.Unit/OutputTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    class OutputTest
+    {
+        private Output _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+          _uut = new Output();
+        }
+
+        [Test]
+        public void 
+
+    }
+}
